e1d8a866403014a740a7b3f97e77b8235264b667
brunomascolo <brunomascolo95@gmail.com>
2022-06-28 23:01:05 -0300

    Implementado registro de usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const fs = require('fs');
+const { equal } = require('assert');
+
+const usersFilePath = path.join(__dirname, '../data/Users.json');
+const user = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 
 const controladorUser = {
@@ -8,6 +12,21 @@ const controladorUser = {
     },
     register:(req,res) => {
         res.render('users/register.ejs')
+    },
+    store:(req,res) =>{
+        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+            let newUser = {
+                id: users[users.length - 1].id + 1,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: req.body.password,
+                category: "user",
+                image: req.file == undefined ? "/img/images/default.jpg" : "/img/images/" + req.file.filename
+            }
+            users.push(newUser);
+            fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+            res.redirect("/user/login")
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,24 @@
 const express = require ('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require("path");
 
+//Configurar multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/img/images/users')
+    },
+    filename: function(req, file, cb){
+        cb(null, "user_" + Date.now() + path.extname(file.originalname))
+    },
+});
+
+const upload = multer({storage: storage});
 
 const userController = require ('../controllers/userController')
 
 router.get('/login', userController.login);
 router.get('/register', userController.register);
+router.post('/', upload.single("img"), userController.store);
 
 module.exports = router;
