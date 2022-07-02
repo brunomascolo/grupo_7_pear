023f0fd6742c9ff2d4fa21dc156ffbf8e02ee1b0
TagPlex <kevinp274@gmail.com>
2022-07-02 12:40:55 -0400

    Opcional de no registrar sin repetir correo funcionando

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const { equal } = require('assert');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
-
 const usersFilePath = path.join(__dirname, '../data/Users.json');
 const user = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
@@ -44,6 +43,19 @@ const controladorUser = {
     },
     store:(req,res) =>{
         const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+        let findUser = function(field, text){
+            let usuarioBuscado = user.find(oneUser => oneUser[field] === text)
+            return usuarioBuscado};
+        let userToLogin = findUser("email", req.body.email)
+        if(userToLogin){
+            return res.render('users/register.ejs', {
+                errors: {
+                    email: {
+                        msg: "El correo registrado ya esta en uso."
+                    }
+                }
+            })
+        }else{
             let newUser = {
                 id: users[users.length - 1].id + 1,
                 first_name: req.body.first_name,
@@ -57,8 +69,8 @@ const controladorUser = {
         users.push(newUser);
         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
         res.redirect("/user/login")
-            
-            
+    
+        }
     },
 
     logout: (req, res)=>{
